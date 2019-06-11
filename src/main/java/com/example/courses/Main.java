24fc2c4e462fe6298d242202fe1f477ef3f95b77
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-06-11 18:43:58 -0500

    Rendered a handlebar index page

--- src/main/java/com/example/courses/Main.java
+++ src/main/java/com/example/courses/Main.java
@@ -1,10 +1,20 @@
 package com.example.courses;
 
+import spark.ModelAndView;
+
 import static spark.Spark.get;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+// http://sparkjava.com/
+
+// Installed an intelij plugin for handlebars to color code files with the HBS extension
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World"); // Find page on http://localhost:4567/hello
-        get("/", (req, res) -> "Welcome");
+        //get("/hello", (req, res) -> "Hello World"); // Find page on http://localhost:4567/hello
+
+        get("/", (req, res) -> {
+            return new ModelAndView(null, "index.hbs");
+        }, new HandlebarsTemplateEngine());     // Renders a handlebar index page
     }
 }
