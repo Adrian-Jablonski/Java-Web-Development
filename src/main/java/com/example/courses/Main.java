d2a7025a6f0c4c0e287a186b5e50a65821d35ffd
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-06-11 19:06:32 -0500

    Stored username in site cookie. Add if code block to .hbs file

--- src/main/java/com/example/courses/Main.java
+++ src/main/java/com/example/courses/Main.java
@@ -19,12 +19,19 @@ public class Main {
         //get("/hello", (req, res) -> "Hello World"); // Find page on http://localhost:4567/hello
 
         get("/", (req, res) -> {
-            return new ModelAndView(null, "index.hbs");
+            Map<String, String> model = new HashMap<>();
+            model.put("username", req.cookie("username"));
+
+            return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());     // Renders a handlebar index page
 
         post("/sign-in", (req, res) -> {
             Map<String, String> model = new HashMap<>();
-            model.put("username", req.queryParams("username")); // Gets the username typed into the form in the index page and assigns it a key of username which will be used to reference it in the sign-in.hbs file
+            String username = req.queryParams("username");
+
+            res.cookie("username", username); // Sets cookie so that username will be available on every page throughout the website
+
+            model.put("username", username); // Gets the username typed into the form in the index page and assigns it a key of username which will be used to reference it in the sign-in.hbs file
 
             return new ModelAndView(model, "sign-in.hbs");
         }, new HandlebarsTemplateEngine());
