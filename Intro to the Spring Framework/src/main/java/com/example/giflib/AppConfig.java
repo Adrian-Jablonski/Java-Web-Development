c0fc089156386dfa0397fc5f46eaa8b9eb3c0681
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-06-14 20:40:12 -0500

    Added gif object to model map and accessed object in HTML using Thymeleaf ${ } syntax

--- Intro to the Spring Framework/src/main/java/com/example/giflib/AppConfig.java
+++ Intro to the Spring Framework/src/main/java/com/example/giflib/AppConfig.java
@@ -2,12 +2,13 @@ package com.example.giflib;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
+@ComponentScan  // Instructs framework to automatically scan current package for controllers
 public class AppConfig {
-
     public static void main(String[] args) {
-        // Creates a spring application that is deployable to a web server
+        // Creates a spring application that is deployable to a web server using Tomcat
         SpringApplication.run(AppConfig.class, args);   // to run server, open gradle window --> Tasks --> application --> bootRun -- Right click and click run. Application is opened on port 8080 (http://localhost:8080/)
 
     }
--- Intro to the Spring Framework/src/main/java/com/example/giflib/Controller/GifController.java
+++ Intro to the Spring Framework/src/main/java/com/example/giflib/Controller/GifController.java
@@ -1,4 +1,39 @@
 package com.example.giflib.Controller;
 
+import com.example.giflib.model.Gif;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
+
+@Controller
 public class GifController {
+//    @RequestMapping(value = "/")    // routes to index page
+//    //@ResponseBody   // Indicates that string we return should be used as the response without any further processing (Not needed when using a template to process response such as thymeleaf
+//    public String listGifs() {
+//        return "index";     // renders index.html
+//    }
+//
+//    @RequestMapping("/gif")
+//    @ResponseBody
+//    public String gifs() {
+//        return "Gifs page";
+//    }
+
+
+    @RequestMapping(value = "/")    // routes to index page
+    //@ResponseBody   // Indicates that string we return should be used as the response without any further processing (Not needed when using a template to process response such as thymeleaf
+    public String listGifs() {
+        return "home";     // renders index.html
+    }
+
+    @RequestMapping("/gif")
+    public String gifDetails(ModelMap modelMap) {
+        Gif gif = new Gif("compiler-bot", LocalDate.of(2015,2,13), "Adrian", true);
+        modelMap.put("gif", gif);   // adds object to model map for it to be available to the templating engine
+
+        return "gif-details";
+    }
 }
