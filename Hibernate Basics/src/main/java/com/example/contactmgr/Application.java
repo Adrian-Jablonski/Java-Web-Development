b4f09d0f95fd99e1a8dc463f3962c5bb5c96dcb5
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-06-17 20:17:11 -0500

    Added builder design pattern

--- Hibernate Basics/src/main/java/com/example/contactmgr/Application.java
+++ Hibernate Basics/src/main/java/com/example/contactmgr/Application.java
@@ -1,7 +1,29 @@
 package com.example.contactmgr;
 
-public class Application {
-    public static void main(String[] args) {
+import com.example.contactmgr.model.Contact;
+import com.example.contactmgr.model.Contact.ContactBuilder;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+
+public class Application {
+    // Hold a reusable reference to a SessionFactory (Since we need only one)
+    //private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        // Create a StandardServiceRegistry
+        final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();  // loads hibernate config file from default location in resources folder (hibernate.cfg.xml)
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();     // builds the session factory. Includes the configuration of all our mappings and includes the configuration for how to connect to the database
+    }
+
+    public static void main(String[] args) {
+        Contact contact = new ContactBuilder("Adrian", "Jablonski")
+        .withEmail("adrian@jab.com")
+        .withPhone(4445558899L)
+                .build();
+
+        System.out.println(contact);
     }
 }
