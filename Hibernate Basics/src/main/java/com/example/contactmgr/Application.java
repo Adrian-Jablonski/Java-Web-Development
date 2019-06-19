a42f0e4cc8eb0c2c5d043bd619dd9b2ff625a0f3
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-06-18 20:32:34 -0500

    Added update method for hibernate database row

--- Hibernate Basics/src/main/java/com/example/contactmgr/Application.java
+++ Hibernate Basics/src/main/java/com/example/contactmgr/Application.java
@@ -30,14 +30,60 @@ public class Application {
                 .build();
 
         System.out.println(contact);
-        save(contact);
+
+        int id = save(contact);
 
         //Display list of contacts
 //        for (Contact c : fetchAllContacts()) {
 //            System.out.println(c);
 //        }
+        System.out.printf("%n%n Before Update %n%n");
         fetchAllContacts().stream().forEach(System.out::println);
 
+        // Get the persisted contact
+        Contact c = findContactById(id);
+
+        // Update the contact
+        c.setFirstName("A");
+
+        // Persist the changes
+        System.out.printf("%n%n Updating %n%n");
+        update(c);
+        System.out.printf("%n%n Update Complete %n%n");
+
+        // Display a list of contacts after the update
+        System.out.printf("%n%n After Update %n%n");
+        fetchAllContacts().stream().forEach(System.out::println);
+    }
+
+    private static Contact findContactById(int id) {
+        //Open a session
+        Session session = sessionFactory.openSession();
+        //Retrieve the persistent object (or null if not found)
+        Contact contact = session.get(Contact.class, id);
+
+        // Close the session
+        session.close();
+
+        // Return the object
+        return contact;
+    }
+
+    private static void update(Contact contact) {
+        // Open a session
+        Session session = sessionFactory.openSession();
+
+        // Begin a transaction
+        session.beginTransaction();
+
+        // Use the session to update the contact
+        session.update(contact);
+
+        // Commit the changes
+        session.getTransaction().commit();
+
+        // Close the session
+        session.close();
     }
 
     private static List<Contact> fetchAllContacts() {
@@ -57,7 +103,7 @@ public class Application {
         return contacts;
     }
 
-    private static void save(Contact contact) {
+    private static int save(Contact contact) {
         // Open a session
         Session session = sessionFactory.openSession();
 
@@ -65,12 +111,14 @@ public class Application {
         session.beginTransaction();
 
         // Use the session to save the contact
-        session.save(contact);
+        int id = (int)session.save(contact);
 
         // Commit the transaction
         session.getTransaction().commit();
 
         // Close session
         session.close();
+
+        return id;
     }
 }
